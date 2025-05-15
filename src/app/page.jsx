bc0e61e15142cc8bc74dc5d6aca7da6ac9aabcfc
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-05-15 08:25:34 -0300

    rafactor: initial configs

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Games Retrô</h1>
+      <p>Recordes de jogos retrô</p>
+    </main>
+  );
+}
